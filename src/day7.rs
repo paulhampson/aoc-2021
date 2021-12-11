6be0348a2868418757027e582c556e6cf8a1a7da
Paul Hampson <528002+paulhampson@users.noreply.github.com>
2021-12-11 15:23:08 +0000

    Day 7 part B completed.

--- src/day7.rs
+++ src/day7.rs
@@ -1,15 +1,4 @@
-use std::collections::HashMap;
-
 use read_file::InputParser;
-
-// fn count_crabs_at_positions(crab_positions: &Vec<usize>) -> HashMap<usize, usize> {
-//     let mut grouped_positions = HashMap::new();
-//     for crab in crab_positions {
-//         *grouped_positions.entry(*crab).or_insert(0) += 1;
-//     }
-//     return grouped_positions;
-// }
-
 
 fn find_desired_position(crab_positions: &Vec<usize>) -> usize {
     // Calculate the median value
@@ -32,11 +21,56 @@ fn get_min_fuel_to_align_crabs(crab_positions: Vec<usize>) -> usize {
     return fuel_used;
 }
 
+fn find_desired_position_b(crab_positions: &Vec<usize>) -> usize {
+    // Calculate the mean value
+    let sum = crab_positions.iter().sum::<usize>();
+    let mean = sum as f32 / crab_positions.len() as f32;
+    println!("True mean: {}", mean);
+    mean.round() as usize
+}
+
+fn calc_fuel_use_for_pos(crab_positions: &Vec<usize>, desired_pos: usize) -> usize {
+    let mut fuel_used: usize = 0;
+    for crab_position in crab_positions {
+        let position_delta: isize = *crab_position as isize - desired_pos as isize;
+        // sum of natural numbers n(n+1)/2 where n=position_delta
+        fuel_used += ((position_delta.abs() * (position_delta.abs() + 1)) / 2) as usize;
+    }
+    return fuel_used;
+}
+
+fn get_min_fuel_to_align_crabs_b(crab_positions: Vec<usize>) -> usize {
+    let desired_pos = find_desired_position_b(&crab_positions);
+    println!("Desired position (mean): {}", desired_pos);
+
+    // A bit of experimentation shows that apparently there's some margin around it being the mean.
+    // This threw me for a while, so I just brute force around the mean.
+    let mut fuel_use_exploration = Vec::new();
+    let exploration_range = 5;
+    let lowest: usize;
+    if desired_pos < exploration_range {
+        lowest = 0;
+    } else {
+        lowest = desired_pos - 5;
+    }
+
+    let highest = desired_pos + exploration_range;
+    for pos in lowest..highest {
+        let fuel_used = calc_fuel_use_for_pos(&crab_positions, pos);
+        println!("{} => {}", pos, fuel_used);
+        fuel_use_exploration.push(fuel_used);
+    }
+    fuel_use_exploration.sort();
+
+    return fuel_use_exploration[0];
+}
+
 pub fn day7a() -> String {
     let crab_positions: Vec<usize> = Vec::read_single_line_csv("./inputs/day7/full".to_string());
     get_min_fuel_to_align_crabs(crab_positions).to_string()
 }
 
 pub fn day7b() -> String {
-    "Nope".to_string()
+    let crab_positions: Vec<usize> = Vec::read_single_line_csv("./inputs/day7/full".to_string());
+    get_min_fuel_to_align_crabs_b(crab_positions).to_string()
 }
